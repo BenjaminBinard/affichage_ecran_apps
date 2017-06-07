d0cede9fc9e7e5b4cf0dd698829ec155b105a631
BenjaminBinard <benjii.binard@gmail.com>
2017-06-07 09:09:21 +0200

    modifié :         .idea/misc.xml 	modifié :         app/src/main/java/com/example/root/myapplication/MainActivity.java 	modifié :         app/src/main/res/layout/activity_main.xml 	supprimé :        test.html

--- app/src/main/java/com/example/root/myapplication/MainActivity.java
+++ app/src/main/java/com/example/root/myapplication/MainActivity.java
@@ -2,12 +2,32 @@ package com.example.root.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.util.Log;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private ToggleButton monbouton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        test();
+    }
+
+    public void test() {
+        monbouton = (ToggleButton) findViewById(R.id.id_test);
+        monbouton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    Log.d(TAG, "Le bouton est allume");
+                } else {
+                    Log.d(TAG, "Le bouton est eteint");
+                }
+            }
+        });
     }
 }
